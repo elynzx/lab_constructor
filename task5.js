8588125a18fc648d430bc6779b459bc85ab17a38
Elynzx <rpascual06@outlook.com>
2026-09-23 16:27:57 -0500

    task5 con operador rest

--- task5.js
+++ task5.js
@@ -1,5 +1,11 @@
 // ¡Escribe tu código debajo de esta línea!
+function FriendList(cantidad, nombres) {
+  this.cantidad = cantidad;
+  this.nombres = nombres;
+}
 
+const [, , , cantidadParam, ...nombresParam] = process.argv;
+const nuevaLista = new FriendList(cantidadParam, nombresParam);
 
-
+console.log(nuevaLista.nombres);
 // ¡Escribe tu código encima de esta línea!
