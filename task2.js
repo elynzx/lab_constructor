14b54b9397008345348fdbe7bf262483d92bc59a
Elynzx <rpascual06@outlook.com>
2026-09-23 15:56:58 -0500

    task2

--- task2.js
+++ task2.js
@@ -1,12 +1,15 @@
 function Mail(asunto, mensaje) {
-  this.asunto = asunto
-  this.mensaje = mensaje
+  this.asunto = asunto;
+  this.mensaje = mensaje;
 }
 
 // ¡Escribe tu código debajo de esta línea!
 
-const nuevoCorreo = new Mail()
+const asuntoParam = process.argv[3];
+const mensajeParam = process.argv[4];
+
+const nuevoCorreo = new Mail(asuntoParam, mensajeParam);
 
 // ¡Escribe tu código encima de esta línea!
 
-console.log(nuevoCorreo.asunto + ": " + nuevoCorreo.mensaje)
+console.log(nuevoCorreo.asunto + ": " + nuevoCorreo.mensaje);
