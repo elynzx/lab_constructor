2cd555e6accd156b2d8b26fb12798552fcca40e7
Elynzx <rpascual06@outlook.com>
2026-09-23 16:06:45 -0500

    task3

--- task3.js
+++ task3.js
@@ -1,13 +1,18 @@
 // ¡Escribe tu código debajo de esta línea!
 
 function Mail(asunto, mensaje) {
-  this.asunto = asunto
-  this.mensaje = mensaje
-
+  this.asunto = asunto;
+  this.mensaje = mensaje;
+  this.imprimirCorreo = function () {
+    console.log(`${this.asunto}: ${this.mensaje}`);
+  };
 }
 
-const nuevoCorreo = new Mail()
+const asuntoParam = process.argv[3];
+const mensajeParam = process.argv[4];
+
+const nuevoCorreo = new Mail(asuntoParam, mensajeParam);
 
 // ¡Escribe tu código encima de esta línea!
 
-nuevoCorreo.imprimirCorreo()
+nuevoCorreo.imprimirCorreo();
