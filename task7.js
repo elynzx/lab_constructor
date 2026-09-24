b6fb469477c80500a5c3afb320923058c594d815
Elynzx <rpascual06@outlook.com>
2026-09-23 21:55:41 -0500

    task7

--- task7.js
+++ task7.js
@@ -1,5 +1,53 @@
 // ¡Escribe tu código debajo de esta línea!
+function Auto(
+  marca,
+  modelo,
+  anio,
+  color,
+  puertas,
+  kilometraje,
+  placa,
+  tipoMotor,
+) {
+  this.marca = marca;
+  this.modelo = modelo;
+  this.anio = anio;
+  this.color = color;
+  this.puertas = puertas;
+  this.kilometraje = kilometraje;
+  this.placa = placa;
+  this.tipoMotor = tipoMotor;
+  this.motor = function () {
+    if (this.tipoMotor == "combustion") {
+      return "combustion";
+    } else {
+      return "electrico";
+    }
+  };
+  this.generarFicha = function () {
+    return `Auto: ${this.marca} ${this.modelo} (${this.anio}), Placa: ${this.placa}, Color: ${this.color}, ${this.puertas} puertas, ${this.kilometraje}km, Motor: ${this.motor()}`;
+  };
+}
 
+const marcaParam = process.argv[2];
+const modeloParam = process.argv[3];
+const anioParam = process.argv[4];
+const colorParam = process.argv[5];
+const puertasParam = process.argv[6];
+const kilometrajeParam = process.argv[7];
+const placaParam = process.argv[8];
+const motorParam = process.argv[9];
 
+const miAuto = new Auto(
+  marcaParam,
+  modeloParam,
+  anioParam,
+  colorParam,
+  puertasParam,
+  kilometrajeParam,
+  placaParam,
+  motorParam,
+);
 
+console.log(miAuto.generarFicha());
 // ¡Escribe tu código encima de esta línea!
